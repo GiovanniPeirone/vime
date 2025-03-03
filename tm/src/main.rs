ca6ef318394ffd0b592821ce45d48dd1fb7e50ec
Giovanni Peirone <mgpeirone25@gmail.com>
2025-03-02 23:02:44 -0300

    a

--- tm/src/main.rs
+++ tm/src/main.rs
@@ -1,46 +1,11 @@
-use std::io;
-use crossterm::{
-    event::{self, KeyCode},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-};
-use tui::{
-    backend::CrosstermBackend,
-    widgets::{Block, Borders},
-    layout::{Layout, Constraint, Direction},
-    Terminal,
-};
+pub use app::App;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Configurar terminal
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
+pub mod app;
 
-    // Dibujar interfaz
-    terminal.draw(|frame| {
-        let size = frame.size();
-        let block = Block::default().title("Hola, TUI!").borders(Borders::ALL);
-        frame.render_widget(block, size);
-    })?;
-
-    // Esperar a que el usuario presione una tecla para salir
-    loop {
-        if let event::Evet::Key(key) = event::read()? {
-            if key.code == KeyCode::Char('q') {
-                break;
-            }
-        }
-
-    }
-
-    
-
-    // Restaurar terminal
-    disable_raw_mode()?;
-    execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
-
-    Ok(())
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let result = App::new().run(terminal);
+    ratatui::restore();
+    result
 }
